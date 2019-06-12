f643453f37e878cea19141832b85980c696a2943
Umesh Patil <uPatil@dhrodc.com>
2019-06-12 23:32:29 +0530

    jenkinsfile.sh-->updated-->master

--- jenkinsfile.sh
+++ jenkinsfile.sh
@@ -4,3 +4,4 @@ stage: deployment --> before merging with devopstesting branch
 =================================================
 stage: sonarqube
 >>>>>>> devopstesting
+stage: deploymentartifacts --> on master
