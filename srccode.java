d0bb963be1cd03345b675b0478db856b71bf1e46
Umesh Patil <uPatil@dhrodc.com>
2019-06-15 12:26:18 +0530

    reset

--- srccode.java
+++ srccode.java
@@ -1,2 +1,7 @@
 This is a source code file created on features branch
+<<<<<<< HEAD
 this is updated on ec2
+=======
+file updated on remote repo
+>>>>>>> d9fdd64c2e79b0211c3b71838ac8fc23674b309f
+accepted
